12877c754d7683d7c45e07cbbf566a86acbb168e
Alex Anderson <andersonaj116@gmail.com>
2023-03-06 09:45:40 -0600

    Initial Commit

---
+++ Fortnite API/Fortnite_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fortnite_APIApp.swift
+//  Fortnite API
+//
+//  Created by Alex Anderson on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Fortnite_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
